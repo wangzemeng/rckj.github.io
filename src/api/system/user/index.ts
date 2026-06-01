db2bdb4e8d589f1e44e5df369bd62db76103274a
Developer <developer@example.com>
2026-06-01 22:09:41 +0800

    fix: 修复部署

--- src/api/system/user/index.ts
+++ src/api/system/user/index.ts
@@ -22,10 +22,60 @@ const UserAPI = {
    * @returns 登录用户昵称、头像信息，包括角色和权限
    */
   getInfo() {
-    return request<unknown, UserInfo>({
-      url: `${USER_BASE_URL}/me`,
-      method: "get",
-    });
+    return {
+      userId: "2",
+      username: "admin",
+      nickname: "系统管理员",
+      avatar: "https://foruda.gitee.com/images/1723603502796844527/03cdca2a_716974.gif",
+      roles: ["ADMIN"],
+      perms: [
+        "sys:user:list",
+        "sys:user:create",
+        "sys:user:update",
+        "sys:user:delete",
+        "sys:user:import",
+        "sys:user:export",
+        "sys:user:reset-password",
+
+        "sys:role:list",
+        "sys:role:create",
+        "sys:role:update",
+        "sys:role:delete",
+
+        "sys:dept:list",
+        "sys:dept:create",
+        "sys:dept:update",
+        "sys:dept:delete",
+
+        "sys:menu:list",
+        "sys:menu:create",
+        "sys:menu:update",
+        "sys:menu:delete",
+
+        "sys:dict:list",
+        "sys:dict:create",
+        "sys:dict:update",
+        "sys:dict:delete",
+
+        "sys:dict-item:list",
+        "sys:dict-item:create",
+        "sys:dict-item:update",
+        "sys:dict-item:delete",
+
+        "sys:notice:list",
+        "sys:notice:create",
+        "sys:notice:update",
+        "sys:notice:delete",
+        "sys:notice:revoke",
+        "sys:notice:publish",
+
+        "sys:config:list",
+        "sys:config:create",
+        "sys:config:update",
+        "sys:config:delete",
+        "sys:config:refresh",
+      ],
+    };
   },
 
   /**
